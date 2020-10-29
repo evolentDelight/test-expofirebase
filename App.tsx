5863b7c9cd614488f3c3b75b1c1cde4cf910e2c2
evolentDelight <evolentDelight@outlook.com>
2020-10-29 00:15:38 -0700

    Test

--- App.tsx
+++ App.tsx
@@ -3,15 +3,15 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 //Screens
-import HomeScreen from "./screen/HomeScreen"
-import Firebase from "./screen/Firebase"
-import TextDetector from "./screen/TextDetector"
-import StackNotes from "./screen/StackNotes"
-import TextEditor from "./screen/TextEditor"
+import HomeScreen from "./screen/HomeScreen";
+import Firebase from "./screen/Firebase";
+import TextDetector from "./screen/TextDetector";
+import StackNotes from "./screen/StackNotes";
+import TextEditor from "./screen/TextEditor";
 
 export default function App() {
   const Stack = createStackNavigator();
-
+  /*TestTest*/
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -28,20 +28,19 @@ export default function App() {
         <Stack.Screen
           name="TextDetector"
           component={TextDetector}
-          options={{title: "TextDetector"}}
+          options={{ title: "TextDetector" }}
         />
         <Stack.Screen
           name="StackNotes"
           component={StackNotes}
-          options={{title: "StackNotes"}}
+          options={{ title: "StackNotes" }}
         />
         <Stack.Screen
           name="TextEditor"
           component={TextEditor}
-          options={{title: "TextEditor"}}
+          options={{ title: "TextEditor" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
